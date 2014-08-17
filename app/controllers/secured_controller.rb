9e43bc1830387d0f48ab59d8dbd5cfec30dc079e
Stephen H. Gerstacker <stephen@shortround.net>
2014-08-16 22:02:54 -0400

    Configuration Files
    - Added configuration files for common settings.
    - Authentication now uses the config file.

--- app/controllers/secured_controller.rb
+++ app/controllers/secured_controller.rb
@@ -1,3 +1,3 @@
 class SecuredController < ApplicationController
-  http_basic_authenticate_with :name => 'admin', :password => 'password'
+  http_basic_authenticate_with :name => APP_CONFIG['username'], :password => APP_CONFIG['password']
 end
